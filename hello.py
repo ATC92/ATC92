20c8d90e68ebe578b9f2d8f5302eed6a56d3885d
ATC92 <a2201636@uabc.edu.mx>
2024-02-03 17:21:51 -0800

    First python code demo

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hola Mundo")
+for x in range(1,5+1,1):
+    print(x)
+if x==5:
+    nombre=input("Ingresa tu nombre: ")
+    edad=input("Ingresa tu edad: ")
+print(f"Hola {nombre} con {edad} años, Buenas tardes! ")
