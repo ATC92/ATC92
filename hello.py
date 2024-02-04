82070015046e10806999769e05579eb22db30dcf
Alan Torres <158106033+ATC92@users.noreply.github.com>
2024-02-03 17:25:57 -0800

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-print("Hola Mundo")
-for x in range(1,5+1,1):
-    print(x)
-if x==5:
-    nombre=input("Ingresa tu nombre: ")
-    edad=input("Ingresa tu edad: ")
-print(f"Hola {nombre} con {edad} años, Buenas tardes! ")
